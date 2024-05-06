f78d50e492d3ae4b6a77be56d1d27e1d0989bf4e
Dan Moffat <danmoffat@gmail.com>
2024-05-06 16:50:25 +0100

    Moved validation to the user model

--- models/user.ts
+++ models/user.ts
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
-
+import Joi from 'joi';
 interface User {
   name: string;
 }
 
 const userSchema = new mongoose.Schema<User>({
   name: { type: String, required: true }
+});
+
+export const validateDeleteUser = Joi.object().keys({
+  id: String
 });
 
 export default mongoose.model<User>('users', userSchema);
--- routes/users.ts
+++ routes/users.ts
@@ -1,7 +1,6 @@
 import express, { type Request, type Response } from 'express';
 import { faker } from '@faker-js/faker';
-import Joi from 'joi';
-import userModel from '../models/user';
+import userModel, { validateDeleteUser } from '../models/user';
 
 const router = express.Router();
 
@@ -33,19 +32,16 @@ router.post('/', async (req: Request, res: Response) => {
 // @desc    Delete a user by id
 // @access  Public
 router.delete('/:id', async (req: Request, res: Response) => {
-  const deleteSchema = Joi.object().keys({
-    id: String
-  });
-
-  const { error } = deleteSchema.validate(req.params);
-
-  if (error) {
-    res.status(422).send({ error: error.details });
-  }
-
-  const { id } = req.params;
-
   try {
+    const { error } = await validateDeleteUser.validateAsync(req.params);
+
+    if (error) {
+      console.log(error);
+      res.status(422).send({ error: error.details });
+    }
+
+    const { id } = req.params;
+
     const response = await userModel.deleteOne({ _id: id });
     res.status(200).send({ id, ...response });
   } catch (error) {
