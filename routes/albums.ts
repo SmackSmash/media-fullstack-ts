628419f0a351940589e05873814d2d67bab9eb6f
Dan Moffat <danmoffat@gmail.com>
2024-05-08 23:34:48 +0100

    Added route to get albums and improved error handling

--- routes/albums.ts
+++ routes/albums.ts
@@ -4,16 +4,24 @@ import { faker } from '@faker-js/faker';
 
 const router = express.Router();
 
+// @route   GET /albums
+// @desc    Get all albums
+// @access  Public
+router.get('/', async (req: Request, res: Response) => {});
+
 // @route   POST /albums
 // @desc    Add an album attached to a user
 // @access  Public
 router.post('/:userId', async (req: Request, res: Response) => {
-  console.log(req.params);
-  const response = await albumModel.create({
-    title: faker.music.songName(),
-    userId: req.params.userId
-  });
-  res.send(response);
+  try {
+    const response = await albumModel.create({
+      title: faker.music.songName(),
+      userId: req.params.userId
+    });
+    res.send(response);
+  } catch (error: unknown) {
+    res.status(500).send(error);
+  }
 });
 
 export default router;
