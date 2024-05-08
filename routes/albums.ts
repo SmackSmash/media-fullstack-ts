1660786b12334c2967d4b73a055c8fd63e25834b
Dan Moffat <danmoffat@gmail.com>
2024-05-08 23:40:43 +0100

    Added route to get all albums belonging to a specific user

--- routes/albums.ts
+++ routes/albums.ts
@@ -16,6 +16,18 @@ router.get('/', async (req: Request, res: Response) => {
   }
 });
 
+// @route   GET /albums/:userId
+// @desc    Get all albums by specific user
+// @access  Public
+router.get('/:userId', async (req: Request, res: Response) => {
+  try {
+    const response = await albumModel.find({ userId: req.params.userId });
+    res.send(response);
+  } catch (error: unknown) {
+    res.status(500).send(error);
+  }
+});
+
 // @route   POST /albums
 // @desc    Add an album attached to a user
 // @access  Public
